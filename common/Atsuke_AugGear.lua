c1a4a81278a9ca78d2b9758b55da31a8e8ba5e4c
Mike <daymi@mail.gvsu.edu>
2022-03-05 20:01:02 -0500

    Gear updates

--- common/Atsuke_AugGear.lua
+++ common/Atsuke_AugGear.lua
@@ -106,35 +106,35 @@
 -- This helps when your augment changes to only have 1 place to update. 
     Helios.Head.BPDmg =   { name="Helios Band", augments={'Pet: "Dbl. Atk."+6','Blood Pact Dmg.+6',}}
 	  
-	Merl.Head.FC      =   { name="Merlinic Hood", augments={'"Mag.Atk.Bns."+27','"Fast Cast"+3','INT+8','Mag. Acc.+1',}}
-	Merl.Body.MAB     =   { name="Merlinic Jubbah", augments={'Accuracy+10','"Mag.Atk.Bns."+30','Accuracy+2 Attack+2','Mag. Acc.+17 "Mag.Atk.Bns."+17',}}
-	Merl.Feet.WSD     =   { name="Merlinic Crackows", augments={'Pet: "Regen"+1','STR+15','Weapon skill damage +7%','Accuracy+4 Attack+4','Mag. Acc.+14 "Mag.Atk.Bns."+14',}}
-	Merl.Hands.MAB    =   { name="Merlinic Dastanas", augments={'"Triple Atk."+2','"Mag.Atk.Bns."+21','Magic Damage +16','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
-	Merl.Hands.BPDmg  =   { name="Merlinic Dastanas", augments={'Pet: Mag. Acc.+21','Blood Pact Dmg.+9','Pet: "Mag.Atk.Bns."+13',}}
-	Merl.Legs.MAB     =   { name="Merlinic Shalwar", augments={'"Mag.Atk.Bns."+11','INT+1','Crit.hit rate+3','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
-	Merl.Feet.Th      =   { name="Merlinic Crackows", augments={'STR+2','Pet: Accuracy+19 Pet: Rng. Acc.+19','"Treasure Hunter"+2','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
-	Merl.Feet.WSD     =   { name="Merlinic Crackows", augments={'Pet: "Regen"+1','STR+15','Weapon skill damage +7%','Accuracy+4 Attack+4','Mag. Acc.+14 "Mag.Atk.Bns."+14',}}
+	Merl.Head.FC      	=   { name="Merlinic Hood", augments={'"Mag.Atk.Bns."+27','"Fast Cast"+3','INT+8','Mag. Acc.+1',}}
+	Merl.Body.MAB     	=   { name="Merlinic Jubbah", augments={'Accuracy+10','"Mag.Atk.Bns."+30','Accuracy+2 Attack+2','Mag. Acc.+17 "Mag.Atk.Bns."+17',}}
+	Merl.Feet.WSD     	=   { name="Merlinic Crackows", augments={'Pet: "Regen"+1','STR+15','Weapon skill damage +7%','Accuracy+4 Attack+4','Mag. Acc.+14 "Mag.Atk.Bns."+14',}}
+	Merl.Hands.MAB    	=   { name="Merlinic Dastanas", augments={'"Triple Atk."+2','"Mag.Atk.Bns."+21','Magic Damage +16','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
+	Merl.Hands.BPDmg  	=   { name="Merlinic Dastanas", augments={'Pet: Mag. Acc.+21','Blood Pact Dmg.+9','Pet: "Mag.Atk.Bns."+13',}}
+	Merl.Legs.MAB     	=   { name="Merlinic Shalwar", augments={'"Mag.Atk.Bns."+11','INT+1','Crit.hit rate+3','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
+	Merl.Feet.Th      	=   { name="Merlinic Crackows", augments={'STR+2','Pet: Accuracy+19 Pet: Rng. Acc.+19','"Treasure Hunter"+2','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
+	Merl.Feet.WSD     	=   { name="Merlinic Crackows", augments={'Pet: "Regen"+1','STR+15','Weapon skill damage +7%','Accuracy+4 Attack+4','Mag. Acc.+14 "Mag.Atk.Bns."+14',}}
 	
-	Apo.Body.A        =   { name="Apo. Dalmatica +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
-	Apo.Feet.A        =   { name="Apogee Pumps +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
-	Apo.Legs.A        =   { name="Apogee Slacks +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
-	Psycloth.Legs.FC  =   { name="Psycloth Lappas", augments={'MP+80','Mag. Acc.+15','"Fast Cast"+7',}}
+	Apo.Body.A        	=   { name="Apo. Dalmatica +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
+	Apo.Feet.A        	=   { name="Apogee Pumps +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
+	Apo.Legs.A        	=   { name="Apogee Slacks +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8',}}
+	Psycloth.Legs.FC  	=   { name="Psycloth Lappas", augments={'MP+80','Mag. Acc.+15','"Fast Cast"+7',}}
 	  
-	Vanya.Head.A      =   { name="Vanya Hood", augments={'MP+50','"Cure" potency +7%','Enmity-6',}}
-	Vanya.Feet.D      =   { name="Vanya Clogs", augments={'"Cure" potency +5%','"Cure" spellcasting time -15%','"Conserve MP"+6',}}
+	Vanya.Head.A      	=   { name="Vanya Hood", augments={'MP+50','"Cure" potency +7%','Enmity-6',}}
+	Vanya.Feet.D      	=   { name="Vanya Clogs", augments={'"Cure" potency +5%','"Cure" spellcasting time -15%','"Conserve MP"+6',}}
 	  
-	Telchine.Head.en  =   { name="Telchine Cap", augments={'Enh. Mag. eff. dur. +9',}}
-	Telchine.Body.en  =   { name="Telchine Chas.", augments={'Enh. Mag. eff. dur. +10',}}
-	Telchine.Hands.en =   { name="Telchine Gloves", augments={'Enh. Mag. eff. dur. +8',}}
-	Telchine.Legs.en  =   { name="Telchine Braconi", augments={'Enh. Mag. eff. dur. +9',}}
-	Telchine.Feet.en  =   { name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +9',}}
+	Telchine.Head.en 	=   { name="Telchine Cap", augments={'Enh. Mag. eff. dur. +9',}}
+	Telchine.Body.en 	=   { name="Telchine Chas.", augments={'Enh. Mag. eff. dur. +10',}}
+	Telchine.Hands.en 	=   { name="Telchine Gloves", augments={'Enh. Mag. eff. dur. +8',}}
+	Telchine.Legs.en  	=   { name="Telchine Braconi", augments={'Enh. Mag. eff. dur. +9',}}
+	Telchine.Feet.en  	=   { name="Telchine Pigaches", augments={'Enh. Mag. eff. dur. +9',}}
 
-	Herc.Head.WSD  = { name= "Herculean Helm", augments={'"Conserve MP"+3','INT+2','Weapon skill damage +8%','Mag. Acc.+1 "Mag.Atk.Bns."+1',}}
-	Herc.Body.WSD  = { name="Herculean Vest", augments={'AGI+8','Pet: Mag. Acc.+14','Weapon skill damage +10%','Accuracy+16 Attack+16',}}
-	Herc.Body.MAB  = { name="Herculean Vest", augments={'Pet: STR+8','"Mag.Atk.Bns."+13','Accuracy+17 Attack+17','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
-	Herc.Hands.WSD = { name="Herculean Gloves", augments={'Weapon skill damage +4%','Accuracy+10',}}
-	Herc.Legs.MAB  = { name="Herculean Trousers", augments={'"Mag.Atk.Bns."+24','Phys. dmg. taken -2%','MND+14',}}
-	Herc.Feet.WSD  = { name="Herculean Boots", augments={'Accuracy+4','Mag. Acc.+27','Weapon skill damage +8%','Mag. Acc.+19 "Mag.Atk.Bns."+19',}}
+	Herc.Head.WSD		= 	{ name= "Herculean Helm", augments={'"Conserve MP"+3','INT+2','Weapon skill damage +8%','Mag. Acc.+1 "Mag.Atk.Bns."+1',}}
+	Herc.Body.WSD  		=	{ name="Herculean Vest", augments={'AGI+8','Pet: Mag. Acc.+14','Weapon skill damage +10%','Accuracy+16 Attack+16',}}
+	Herc.Body.MAB  		= 	{ name="Herculean Vest", augments={'Pet: STR+8','"Mag.Atk.Bns."+13','Accuracy+17 Attack+17','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
+	Herc.Hands.WSD 		=	{ name="Herculean Gloves", augments={'Weapon skill damage +4%','Accuracy+10',}}
+	Herc.Legs.MAB  		=	{ name="Herculean Trousers", augments={'"Mag.Atk.Bns."+24','Phys. dmg. taken -2%','MND+14',}}
+	Herc.Feet.WSD  		=	{ name="Herculean Boots", augments={'Accuracy+4','Mag. Acc.+27','Weapon skill damage +8%','Mag. Acc.+19 "Mag.Atk.Bns."+19',}}
 
 	Taeon.Head.Phalanx	=	{ name="Taeon Chapeau", augments={'DEF+3','"Conserve MP"+5','Phalanx +3',}}
 	Taeon.Head.TP		=	{ name="Taeon Chapeau", augments={'Accuracy+20 Attack+20','"Triple Atk."+2','STR+5 DEX+5',}}
